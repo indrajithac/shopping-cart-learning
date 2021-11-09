3c63908a9213cbd741fc95364ec5334ebccf6315
Indrajith A C <indrajithac@gmail.com>
2021-11-09 12:17:14 +0530

    view product page is fully based on db

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,4 +1,5 @@
 var db=require('../config/connection')
+var collection=require('../config/collections')
 module.exports={
     addProduct:(product,callback)=>{
         //console.log(product)
@@ -7,5 +8,11 @@ module.exports={
             callback(data.insertedId)            
         })
 
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
     }
 }
