5291c41328757eed48cb2102def427d5fbf5d407
Indrajith A C <indrajithac@gmail.com>
2021-11-10 17:53:35 +0530

    added real products and corrected admin and user first page

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,18 @@
 var express = require('express');
 const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
-var productHelper = require('../helpers/product-helpers')
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   productHelpers.getAllProducts().then((products)=>{
-    console.log(products);
+    //console.log(products);
     res.render('admin/view-products', { admin: true, products })
   })
 });
+
 router.get('/add-product', (req, res) => {
-  res.render('admin/add-product')
+  res.render('admin/add-product',{admin:true})
 
 })
 router.post('/add-product', (req, res) => {
@@ -19,12 +20,12 @@ router.post('/add-product', (req, res) => {
   //console.log(req.files.image)
 
   productHelpers.addProduct(req.body, (result) => {
-    res.render("admin/add-product")
+    //res.render("admin/add-product",{admin:true})
     let image = req.files.image
     //console.log(result);
     image.mv('./public/product-images/' + result + '.jpg', (err, done) => {
       if (!err) {
-        res.render("admin/add-product")
+        res.render("admin/add-product",{admin:true})
       }
 
     })
